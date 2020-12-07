f8c9f2a4803001b0dc9616526fb082ee4ace9918
MuhammadAlGhali <mghali2000@hotmail.com>
2020-12-07 05:39:06 +0200

    set up json-server

---
+++ public/precache-manifest.7a76092703d22943896ba2c6836e1278.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf7c44ca3546e774299f",
+    "url": "/static/css/main.79bde364.chunk.css"
+  },
+  {
+    "revision": "bf7c44ca3546e774299f",
+    "url": "/static/js/main.79ce2281.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bf877fdabd0acc35223c",
+    "url": "/static/css/2.41b19124.chunk.css"
+  },
+  {
+    "revision": "bf877fdabd0acc35223c",
+    "url": "/static/js/2.a22167c4.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "bed8072642944201021595af63f7d716",
+    "url": "/index.html"
+  }
+];
